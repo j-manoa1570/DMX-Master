b71456a617099efb197300738a62de5ce13e642c
j.manoa1570 <jester1570@gmail.com>
2018-12-14 21:42:29 -0700

    Figured out how to add addition view controller classes to scenes.

---
+++ DMX Master/DMX Master/MenuViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MenuViewController.swift
+//  DMX Master
+//
+//  Created by Jonathan Manoa on 12/14/18.
+//  Copyright © 2018 ComboBreaker. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuViewController : UIViewController {
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var loadButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
